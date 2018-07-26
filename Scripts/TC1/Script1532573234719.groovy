8a08a0ca0fe8c0a9403321ae9daa407654f2582c
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-07-26 14:03:31 +0900

    add draft

---
+++ Scripts/TC1/Script1532573234719.groovy
@@ -0,0 +1,26 @@
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+
+def version = '0.2'
+
+def gitReposUrl = "https://github.com/kazurayam/MyCustomKeywords/archive/${version}.zip"
+
+WebUI.comment(">>> username=${GlobalVariable.GitHubUsername}")
+
+CustomKeywords.'shouldbebuiltin.keyword.Portability.includeCustomKeywords'(
+	gitReposUrl,
+	GlobalVariable.GitHubUsername,
+	GlobalVariable.GitHubPassword, 
+    ['com.kazurayam.ksbackyard'])
+
