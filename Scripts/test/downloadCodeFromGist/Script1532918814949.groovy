7eb5cadf57597c69915e41bf0047382a1b27b925
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-07-30 13:35:36 +0900

    update downloadCodeFromGist

--- Scripts/test/downloadCodeFromGist/Script1532918814949.groovy
+++ Scripts/test/downloadCodeFromGist/Script1532918814949.groovy
@@ -7,9 +7,9 @@
  *  @author kazurayam
  */
 // I you are behind the proxy of your orgianization, uncomment the following lines to set proxy config
-//System.setProperty("proxySet", "true");
-//System.setProperty("proxyHost", yourProxyIpAddress);
-//System.setProperty("proxyPort", yourProxyPortNumber);
+System.setProperty("proxySet", "true");
+System.setProperty("proxyHost", "172.24.2.10");
+System.setProperty("proxyPort", "8080");
 
 new File("Keywords/helloWorld.groovy") << new URL(
 	"https://gist.githubusercontent.com/kazurayam/a80a93e4376eae3164adb8e021b78385/raw/f3cc41c07fec95a8840f716ffb931337154c7010/hellowWorld.groovy"
