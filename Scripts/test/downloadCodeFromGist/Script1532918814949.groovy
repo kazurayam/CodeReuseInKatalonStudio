27285ef063698e5751bc8d580b305d033f0c5eff
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-07-30 11:52:30 +0900

    renamed downloadHelloWorld to downloadCodeFromGist

--- Scripts/test/downloadCodeFromGist/Script1532918814949.groovy
+++ Scripts/test/downloadCodeFromGist/Script1532918814949.groovy
@@ -1,3 +1,11 @@
+/**
+ * This one liner Groovy script shows how to download a file and save it anywhere you want.
+ * For example, the following script downloads a Groovy script from the Gist, and store it
+ * into the Keywords directory of this Katalon Project.
+ * This implies that a Keyword posted into Gist is quite easily shared by others.
+ * 
+ *  @author kazurayam
+ */
 // I you are behind the proxy of your orgianization, uncomment the following lines to set proxy config
 //System.setProperty("proxySet", "true");
 //System.setProperty("proxyHost", yourProxyIpAddress);
